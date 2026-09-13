4c96ad654a6e21e7b635643d4a354ab1f5a4ee87
Lavida Yuthiana Faizah <lavida.yuthiana@ui.ac.id>
2026-09-13 16:01:22 +0700

    feat: add views+urls volunteer

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from main.models import Experience
+from main.models import Education
+from main.models import Volunteer
 
 
 def show_main(request):
@@ -22,3 +24,17 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Lavida Yuthiana Faizah",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
+
+def show_volunteer(request):
+    context = {
+        "name": "Lavida Yuthiana Faizah",
+        "volunteer_list": Volunteer.objects.all(),
+    }
+    return render(request, "volunteer.html", context)
