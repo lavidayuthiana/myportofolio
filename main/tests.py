dd6adce95316b6201bf226d099f7b0385ca4d1a3
Lavida Yuthiana Faizah <lavida.yuthiana@ui.ac.id>
2026-09-13 15:23:10 +0700

    feat: add unitest

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 class MainTest(TestCase):
@@ -56,3 +56,30 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+class EducationTest(TestCase):
+    def setUp(self):
+        self.education = Education.objects.create(
+            institution_name="Fakultas Ilmu Komputer UI",
+            degree="S1 Sistem Informasi",
+            score_label="ipk",
+            score_value="3.37",
+        )
+
+    def test_education_url_is_accessible_and_uses_correct_template(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+
+    def test_education_data_appears_on_page(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, self.education.institution_name)
+        self.assertContains(response, self.education.degree)
+
+    def test_empty_education_page(self):
+        Education.objects.all().delete()
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, "Belum ada education yang ditambahkan.")
