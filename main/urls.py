f5f1dbb55f656656c09af0dbdfaee5d4d84bcd5d
Lavida Yuthiana Faizah <lavida.yuthiana@ui.ac.id>
2026-09-23 21:15:59 +0700

    feat: add register, login, logout

--- main/urls.py
+++ main/urls.py
@@ -17,12 +17,19 @@ from main.views import (
     get_experience_json,
     edit_experience,
     delete_experience,
+    register,
+    login_user,
+    logout_user,
 )
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
+    
     path("experience/", show_experience, name="show_experience"),
     path("experience/add/", create_experience, name="create_experience"),
     path("experience/api/", get_experience_json, name="get_experience_json"),
--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
+import datetime
+
 from django.contrib import messages
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from main.forms import EducationForm, VolunteerForm, ExperienceForm
 from main.models import Education, Experience, Volunteer
@@ -18,6 +22,46 @@ def show_main(request):
         ),
     }
     return render(request, "index.html", context)
+
+# ----------------------------- Auth -----------------------------
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Akun berhasil dibuat. Silakan login.")
+        return redirect("main:login")
+
+    context = {
+        "name": "Lavida Yuthiana Faizah",
+        "form": form,
+    }
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = redirect("main:show_main")
+        response.set_cookie('last_login', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        return response
+
+    context = {
+        "name": "Lavida Yuthiana Faizah",
+        "form": form,
+    }
+    return render(request, "login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    response = redirect("main:show_main")
+    response.delete_cookie('last_login')
+    return response
 
 # ----------------------------- Experience -----------------------------
 
